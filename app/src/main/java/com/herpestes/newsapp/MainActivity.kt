f8d51d33cb768e38c864789030880521990904ab
herpestes <ahmetbeyazoglu@gmail.com>
2023-09-01 23:29:00 +0300

    SystemUIController

--- app/src/main/java/com/herpestes/newsapp/MainActivity.kt
+++ app/src/main/java/com/herpestes/newsapp/MainActivity.kt
@@ -6,18 +6,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.lifecycleScope
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.herpestes.newsapp.domain.usercases.AppEntryUseCases
 import com.herpestes.newsapp.domain.usercases.SaveAppEntry
 import com.herpestes.newsapp.presentation.nvgraph.NavGraph
@@ -45,6 +49,17 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             NewsAppTheme {
+
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemController = rememberSystemUiController()
+
+                SideEffect {
+                    systemController.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemInDarkMode
+                    )
+                }
+
                 Box(modifier = Modifier.background(color = MaterialTheme.colors.background)){
                     val startDestination = viewModel.startDestination
                     NavGraph(startDestination = startDestination)
